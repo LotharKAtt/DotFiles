5fc3b7c4922bf72e600605f7e84ce8cc4220b8d9
Pavel Cizinsky <pavel.cizinsky@gmail.com>
2025-02-23 16:27:25 +0100

    Sync dotfiles

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,13 @@
--- _             _    _                   _   __  ___   _    _
---| |           | |  | |                 | | / / / _ \ | |  | |
---| |      ___  | |_ | |__    __ _  _ __ | |/ / / /_\ \| |_ | |_
---| |     / _ \ | __|| '_ \  / _` || '__||    \ |  _  || __|| __|
---| |____| (_) || |_ | | | || (_| || |   | |\  \| | | || |_ | |_
---\_____/ \___/  \__||_| |_| \__,_||_|   \_| \_/\_| |_/ \__| \__|
+--  _             _    _                   _   __  ___   _    _
+-- | |           | |  | |                 | | / / / _ \ | |  | |
+-- | |      ___  | |_ | |__    __ _  _ __ | |/ / / /_\ \| |_ | |_
+-- | |     / _ \ | __|| '_ \  / _` || '__||    \ |  _  || __|| __|
+-- | |____| (_) || |_ | | | || (_| || |   | |\  \| | | || |_ | |_
+-- \_____/ \___/  \__||_| |_| \__,_||_|   \_| \_/\_| |_/ \__| \__|
 --
 -- Author: Pavel 'LotharKAtt' Cizinsky
 -- Repository: github.com/LotharKAtt/DotFiles
 -- wezterm config file (https://github.com/wez/wezterm)
-
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
@@ -21,10 +20,8 @@ local config = wezterm.config_builder()
 -- https://wezfurlong.org/wezterm/colorschemes/t/index.html#tokyo-night-day
 config.color_scheme = 'Tokyo Night Day'
 
-
 config.font_size = 18
 config.enable_csi_u_key_encoding = true
-
 
 -- Disable tabs
 config.enable_tab_bar = false
@@ -41,43 +38,42 @@ config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 -- Hyperlink fix
-config.hyperlink_rules = {
-      -- Matches: a URL in parens: (URL)
-      {
-         regex = '\\((\\w+://\\S+)\\)',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in brackets: [URL]
-      {
-         regex = '\\[(\\w+://\\S+)\\]',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in curly braces: {URL}
-      {
-         regex = '\\{(\\w+://\\S+)\\}',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in angle brackets: <URL>
-      {
-         regex = '<(\\w+://\\S+)>',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Then handle URLs not wrapped in brackets
-      {
-         regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-         format = '$0',
-      },
-      -- implicit mailto link
-      {
-         regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
-         format = 'mailto:$0',
-      },
-   }
-
+config.hyperlink_rules = { -- Matches: a URL in parentheses (URL)
+{
+    regex = '\\((https?://[\\S]+)\\)',
+    format = '$1',
+    highlight = 1
+}, -- Matches: a URL in square brackets [URL]
+{
+    regex = '\\[([\\w+://\\S]+)\\]',
+    format = '$1',
+    highlight = 1
+}, -- Matches: a URL in curly braces {URL}
+{
+    regex = '\\{(https?://[\\S]+)\\}',
+    format = '$1',
+    highlight = 1
+}, -- Matches: a URL in angle brackets <URL>
+{
+    regex = '<(https?://[\\S]+)>',
+    format = '$1',
+    highlight = 1
+}, -- Generic URL matcher (http:// or https://)
+{
+    regex = '\\b(https?://[\\S]+)\\b',
+    format = '$1',
+    highlight = 1
+}, -- Implicit mailto link (email addresses)
+{
+    regex = '\\b([\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,})\\b',
+    format = 'mailto:$0',
+    highlight = 1
+}, -- Matches: a bare domain name or IP (without protocols)
+{
+    regex = '\\b([\\w.-]+(?:\\.\\w+)+)\\b',
+    format = 'http://$0',
+    highlight = 1
+}}
 
 -- Performance
 config.animation_fps = 1
